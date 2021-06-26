fccd97eaf941800a7f0d7b05b6dd8d14b7d7e328
Komatsu Nobuyuki <abr0811@icloud.com>
2021-06-26 19:18:45 +0900

    カラースキーマを定義

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
+const colors = {
+  primary: '#FF6464',
+  secondary: '#00A8CC',
+  dark: '#21243D',
+  light: '#8695A4',
+}
+
 module.exports = {
   purge: ['./pages/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: colors.primary,
+      secondary: colors.secondary,
+      dark: colors.dark,
+      light: colors.light,
+    },
+    textColor: {
+      primary: colors.primary,
+      secondary: colors.secondary,
+      dark: colors.dark,
+      light: colors.light,
+    },
     extend: {},
   },
   variants: {
