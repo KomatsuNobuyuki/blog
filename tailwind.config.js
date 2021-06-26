7eb798bfcdf7079766784a476695ecb34368ac30
Komatsu Nobuyuki <abr0811@icloud.com>
2021-06-26 20:36:30 +0900

    .containerをカスタム

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,5 +26,35 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  corePlugins: {
+    container: false
+  },
+  plugins: [
+    function({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '640px',
+          padding: '0 2rem',
+          '@screen sm': {
+          },
+          '@screen md': {
+            maxWidth: '682px',
+            padding: '0',
+          },
+          '@screen lg': {
+            maxWidth: '682px',
+            padding: '0',
+          },
+          '@screen xl': {
+            maxWidth: '682px',
+            padding: '0',
+          },
+          '@screen 2xl': {
+            maxWidth: '682px',
+            padding: '0',
+          },
+        }
+      })
+    }
+  ],
 }
