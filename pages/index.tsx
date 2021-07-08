dcf11e51e0196beb9b2ca2c442860828aacf223c
Komatsu Nobuyuki <abr0811@icloud.com>
2021-07-08 19:54:01 +0900

    日付の整形と、記事本文のHTMLタグ削除処理をgetStaticProps内で行うように修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { Article } from '@/types/Article';
+import { MicroCMSResponse } from '@/types/MicroCMSResponse';
 import { getFormatedDate } from '@/utils/getFormetedDate';
 
 type HomeProps = {
@@ -11,20 +12,14 @@ export default function Home({ articles }: HomeProps) {
     <div>
       <ul>
         {articles.map(article => {
-          const { createdAt } = article;
-          const date = new Date(createdAt);
-          const formatedDate = getFormatedDate(date);
-          const contents = article.contents;
-          const regex = /<("[^"]*"|'[^']*'|[^'">])[^<>]*>/g;
-          const removedHtmlTagsContents = contents.replace(regex, '');
           return (
           <li key={article.id} className="border-light border-b-2 mt-4 first:mt-0">
             <Link href={`/article/${article.id}`}>
               <a className="pb-8 block hover:opacity-70">
                 <h2 className="text-2xl">{article.title}</h2>
                 <div className="mt-4">
-                  <p>{formatedDate}</p>
-                  <p className="mt-4 line-clamp-2">{removedHtmlTagsContents}</p>
+                  <p>{article.createdAt}</p>
+                  <p className="mt-4 line-clamp-2">{article.contents}</p>
                 </div>
               </a>
             </Link>
@@ -56,12 +51,25 @@ export const getStaticProps = async() => {
         throw new Error(res.statusText);
       }
 
-      return res.json();
+      return res.json() as Promise<MicroCMSResponse<Article>>;
+    });
+
+    const articles = fetchArticle.contents.map((article: Article) => {
+      const { createdAt } = article;
+      const date = new Date(createdAt);
+      const formatedDate = getFormatedDate(date);
+
+      article.createdAt = formatedDate;
+      const regex = /<("[^"]*"|'[^']*'|[^'">])[^<>]*>/g;
+      const removedHtmlTagsContents = article.contents.replace(regex, '');
+      article.contents = removedHtmlTagsContents;
+
+      return article;
     });
 
   return {
     props: {
-      articles: fetchArticle.contents,
+      articles,
     }
   }
 }
