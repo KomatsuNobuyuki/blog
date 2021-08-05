6c66bf3e84a2b70c9c5c167c52f45e9f92f41be0
smallpine8 <nkomatsu.dev@gmail.com>
2021-08-05 14:06:35 +0900

    tailwind.config修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ const colors = {
 }
 
 module.exports = {
-  purge: ['./pages/**/*.tsx'],
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
