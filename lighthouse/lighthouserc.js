a68bef6985200d4b2dc80a8426f9962c9dc5d275
smallpine8 <nkomatsu.dev@gmail.com>
2021-08-10 18:13:26 +0900

    lighthouseのconfigはPromiseを返せないみたいなので、修正

--- lighthouse/lighthouserc.js
+++ lighthouse/lighthouserc.js
@@ -1,35 +1,11 @@
-module.exports = async function() {
-  const configureLHCI = {
-    ci: {
-      collect: {
-        url: ['http://localhost:3000/'],
-        startServerCommand: 'npm start'
-      },
-      upload: {
-        target: 'temporary-public-storage',
-      }
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'npm start'
+    },
+    upload: {
+      target: 'temporary-public-storage',
     }
   }
-
-  // const key = {
-  //   headers: {
-  //     'X-API-KEY': process.env.API_KEY,
-  //   }
-  // }
-
-  // const reqUrl = process.env.REQUEST_URL + 'article';
-
-  // const fetchArticle = await fetch(reqUrl, key)
-  // .catch((e) => { console.error(e) })
-  // .then((res) => {
-  //   return res.json();
-  // });
-
-  // fetchArticle.contents.forEach((article) => {
-  //   const id = { article }
-  //   const collectUrl = `http://localhost:3000/article/${id}`;
-  //   configureLHCI.ci.collect.url.push(collectUrl);
-  // });
-  console.log('configureLHCI: ' + configureLHCI.ci.collect.url);
-  return configureLHCI;
 };
