bb059d769b670dcb4df3a57a0dd8cc509f8db58d
smallpine8 <nkomatsu.dev@gmail.com>
2021-08-10 19:14:56 +0900

    テスト

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <header className="py-10">
       <div className="container mx-auto text-center">
         <Link href="/">
-          <a className="header-title">smallpine8 blog</a>
+          <a className="header-title">smallpine8 blogテスト</a>
         </Link>
       </div>
     </header>
