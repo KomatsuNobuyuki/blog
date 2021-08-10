d8af242d8be86988850c1a3f4af0033a5b50b389
smallpine8 <nkomatsu.dev@gmail.com>
2021-08-10 17:06:04 +0900

    記事詳細ページも集計できるように処理を追加

--- lighthouse/lighthouserc.js
+++ lighthouse/lighthouserc.js
@@ -1,4 +1,4 @@
-module.exports = {
+const configureLHCI = {
   ci: {
     collect: {
       url: ['http://localhost:3000/'],
@@ -9,3 +9,27 @@ module.exports = {
     }
   }
 }
+
+(async() => {
+  const key = {
+    headers: {
+      'X-API-KEY': process.env.API_KEY,
+    }
+  }
+
+  const reqUrl = process.env.REQUEST_URL + 'article';
+
+  const fetchArticle = await fetch(reqUrl, key)
+  .catch((e) => { console.error(e) })
+  .then((res) => {
+    return res.json();
+  });
+
+  fetchArticle.contents.forEach((article) => {
+    const id = { article }
+    const collectUrl = `http://localhost:3000/article/${id}`;
+    configureLHCI.ci.collect.url.push(collectUrl);
+  });
+});
+
+module.exports = configureLHCI;
